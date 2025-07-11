f9ca0bc042f3d3d38bd7112b55c4a74626af37a5
Harold Negrete <negreteharold@gmail.com>
2025-07-10 23:33:30 -0500

    Habilitar Gateway

---
+++ src/main/java/com/linktic/prueba/gateway/GatewayCompraAppApplication.java
@@ -0,0 +1,13 @@
+package com.linktic.prueba.gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GatewayCompraAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GatewayCompraAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/linktic/prueba/gateway/GatewayCompraAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.linktic.prueba.gateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GatewayCompraAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
